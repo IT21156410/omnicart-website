dc16021dcaed66c3de4063558e3d873cb9c4540f
hansajith18 <hansajith18@gmail.com>
2024-10-08 16:27:34 +0530

    Dashboard page design

--- src/app/dashboard/Page.tsx
+++ src/app/dashboard/Page.tsx
@@ -1,54 +1,63 @@
-import {Button, Card, Col, Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
+import {Suspense} from "react";
+import {Card, Skeleton} from "antd";
+import {useAuth} from "../../hooks/useAuth.tsx";
+import corporatePng from "../../assets/corporate.png"
 
 const Dashboard = () => {
+    const {user: authUser} = useAuth();
 
     return (
         <>
-            <Row>
-                <Col md={4}>
-                    <Card className="mb-4 shadow-sm">
-                        <Card.Body>
-                            <Card.Title>Earnings</Card.Title>
-                            <Card.Text className="fs-3">$63,438.78</Card.Text>
-                            <Button variant="primary">Download</Button>
-                        </Card.Body>
-                    </Card>
+            <Row gutter={25}>
+                <Col xs={24}>
+                    <Suspense
+                        fallback={
+                            <Card>
+                                <Skeleton active/>
+                            </Card>
+                        }
+                    >
+                        <Card style={{padding: "30px 40px", backgroundColor: "rgb(32, 36, 68)"}}>
+                            <figure style={{position: "relative", minHeight: 180}}>
+
+                                <img src={corporatePng} alt="img" style={{
+                                    position: "absolute",
+                                    bottom: "50%",
+                                    marginBottom: -80,
+                                    maxWidth: 240,
+                                    right: 30,
+                                }}/>
+                                <figcaption>
+                                    <h2 className="mb-4" style={{fontSize: 30, fontWeight: 600, color: "#ffffff",}}>
+                                        {`Welcome To ${authUser?.role.toLocaleUpperCase()} Dashboard : ${authUser?.name || 'Default user'}`}
+                                    </h2>
+                                    <p style={{
+                                        fontSize: 15,
+                                        maxWidth: 610,
+                                        opacity: 1,
+                                        marginBottom: 25,
+                                        color: "#ffffff",
+                                    }}>
+                                        Manage everything
+                                    </p>
+
+                                </figcaption>
+                            </figure>
+                        </Card>
+
+                    </Suspense>
                 </Col>
-                <Col md={2}>
-                    <Card className="mb-4 shadow-sm">
-                        <Card.Body>
-                            <Card.Title>Customers</Card.Title>
-                            <Card.Text className="fs-4">39,354</Card.Text>
-                            <span className="text-danger">-9%</span>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={2}>
-                    <Card className="mb-4 shadow-sm">
-                        <Card.Body>
-                            <Card.Title>Products</Card.Title>
-                            <Card.Text className="fs-4">4,396</Card.Text>
-                            <span className="text-success">+23%</span>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={2}>
-                    <Card className="mb-4 shadow-sm">
-                        <Card.Body>
-                            <Card.Title>Sales</Card.Title>
-                            <Card.Text className="fs-4">423,39</Card.Text>
-                            <span className="text-success">+38%</span>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={2}>
-                    <Card className="mb-4 shadow-sm">
-                        <Card.Body>
-                            <Card.Title>Refunds</Card.Title>
-                            <Card.Text className="fs-4">835</Card.Text>
-                            <span className="text-danger">-12%</span>
-                        </Card.Body>
-                    </Card>
+                <Col xxl={8} xl={10} xs={24}>
+                    <Suspense
+                        fallback={
+                            <Card>
+                                <Skeleton active/>
+                            </Card>
+                        }
+                    >
+                        {/*<Calender />*/}
+                    </Suspense>
                 </Col>
             </Row>
         </>
